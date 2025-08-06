57739cb67de0634fad345dda8afd8752c0e9c786
Gastón Díaz <diazgastonj@gmail.com>
2025-08-06 10:35:55 -0300

    Completado guia1 ej2

--- firmware/projects/guia1_ej2/main/guia1_ej2.c
+++ firmware/projects/guia1_ej2/main/guia1_ej2.c
@@ -26,14 +26,36 @@
 /*==================[inclusions]=============================================*/
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "led.h"
+#include "switch.h"
 /*==================[macros and definitions]=================================*/
-
+#define CONFIG_BLINK_PERIOD 500
 /*==================[internal data definition]===============================*/
 
 /*==================[internal functions declaration]=========================*/
 
 /*==================[external functions definition]==========================*/
 void app_main(void){
-	printf("Hello world!\n");
+	uint8_t teclas;
+	LedsInit();
+	SwitchesInit();
+    while(1)    {
+    	teclas  = SwitchesRead();
+    	switch(teclas){
+    		case SWITCH_1:
+    			LedToggle(LED_1);
+    		break;
+    		case SWITCH_2:
+    			LedToggle(LED_2);
+    		break;
+			case SWITCH_1 | SWITCH_2:
+				LedToggle(LED_3);
+			break;
+    	}
+	    //LedToggle(LED_3);
+		vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
+	}
 }
-/*==================[end of file]============================================*/
