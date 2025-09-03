39eadb0a780e6d968dd054b5ab0555d7a4abac31
Gastón Díaz <diazgastonj@gmail.com>
2025-09-03 09:50:31 -0300

    Creo proyecto de guia 1 ejercicio 5

---
+++ firmware/projects/guia1_ej5/main/guia1_ej5.c
@@ -0,0 +1,39 @@
+/*! @mainpage guia1_ej5
+ *
+ * @section genDesc General Description
+ *
+ * This section describes how the program works.
+ *
+ * <a href="https://drive.google.com/...">Operation Example</a>
+ *
+ * @section hardConn Hardware Connection
+ *
+ * |    Peripheral  |   ESP32   	|
+ * |:--------------:|:--------------|
+ * | 	PIN_X	 	| 	GPIO_X		|
+ *
+ *
+ * @section changelog Changelog
+ *
+ * |   Date	    | Description                                    |
+ * |:----------:|:-----------------------------------------------|
+ * | 03/09/2025 | Document creation		                         |
+ *
+ * @author Gaston Diaz (diazgastonj@gmail.com)
+ *
+ */
+
+/*==================[inclusions]=============================================*/
+#include <stdio.h>
+#include <stdint.h>
+/*==================[macros and definitions]=================================*/
+
+/*==================[internal data definition]===============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[external functions definition]==========================*/
+void app_main(void){
+	printf("Hello world!\n");
+}
+/*==================[end of file]============================================*/
