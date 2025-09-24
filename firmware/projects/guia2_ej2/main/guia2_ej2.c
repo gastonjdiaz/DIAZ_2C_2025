93b64a7b2cca998825f32633e1f945872966212b
Gastón Díaz <diazgastonj@gmail.com>
2025-09-24 10:35:01 -0300

    Genero proyecto de guia2_ej2

---
+++ firmware/projects/guia2_ej2/main/guia2_ej2.c
@@ -0,0 +1,39 @@
+/*! @mainpage guia2_ej2
+ *
+ * @section genDesc General Description
+ *
+ * This section describes how the program works.
+ *
+ * <a href="https://drive.google.com/...">Operation Example</a>
+ *
+ * @section hardConn Hardware Connection
+ *
+ * |    Peripheral  |   ESP32   	|
+ * |:--------------:|:--------------|
+ * | 	PIN_X	 	| 	GPIO_X		|
+ *
+ *
+ * @section changelog Changelog
+ *
+ * |   Date	    | Description                                    |
+ * |:----------:|:-----------------------------------------------|
+ * | 24/09/2025 | Document creation		                         |
+ *
+ * @author Gastón Díaz (diazgastonj@gmail.com)
+ *
+ */
+
+/*==================[inclusions]=============================================*/
+#include <stdio.h>
+#include <stdint.h>
+/*==================[macros and definitions]=================================*/
+
+/*==================[internal data definition]===============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[external functions definition]==========================*/
+void app_main(void){
+	printf("Hello world!\n");
+}
+/*==================[end of file]============================================*/
