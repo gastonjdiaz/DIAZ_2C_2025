f3cdccb3adaad2da9eb641faa800f2900a3a1d57
Gastón Díaz <diazgastonj@gmail.com>
2025-10-01 10:06:58 -0300

    guia2_ej2: Cambio los task delay por timers

--- firmware/projects/guia2_ej2/main/guia2_ej2.c
+++ firmware/projects/guia2_ej2/main/guia2_ej2.c
@@ -40,8 +40,9 @@
 #define CONFIG_SWITCH_READ_PERIOD 100
 #define CONFIG_MEASUREMENT_PERIOD 1000
 #define CONFIG_BLINK_PERIOD_LED_3 500
+
+#define CONFIG_MEASUREMENT_PERIOD_US 1000 * 1000
 /*==================[internal data definition]===============================*/
-TaskHandle_t LeerTeclas_task_handle = NULL;
 TaskHandle_t Medir_task_handle = NULL;
 TaskHandle_t EncenderLEDs_task_handle = NULL;
 TaskHandle_t ControlLCD_task_handle = NULL;
@@ -59,7 +60,8 @@ static void Medir(void *pvParameter)
         if (_medicionActivada)
             _distanciaCentimetros = HcSr04ReadDistanceInCentimeters();
 
-        vTaskDelay(CONFIG_MEASUREMENT_PERIOD / portTICK_PERIOD_MS);
+       // vTaskDelay(CONFIG_MEASUREMENT_PERIOD / portTICK_PERIOD_MS);
+	   ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
     }
 }
 
@@ -101,7 +103,8 @@ static void EncenderLEDs(void *pvParameter)
             LedOff(LED_3);
         }
 
-        vTaskDelay(CONFIG_MEASUREMENT_PERIOD / portTICK_PERIOD_MS);
+        //vTaskDelay(CONFIG_MEASUREMENT_PERIOD / portTICK_PERIOD_MS);
+		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
     }
 }
 static void ControlLCD(void *pvParameter)
@@ -116,7 +119,8 @@ static void ControlLCD(void *pvParameter)
         {
             LcdItsE0803Write(_distanciaCentimetros);
         }
-        vTaskDelay(CONFIG_MEASUREMENT_PERIOD / portTICK_PERIOD_MS);
+        //vTaskDelay(CONFIG_MEASUREMENT_PERIOD / portTICK_PERIOD_MS);
+		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
     }
 }
 void Tecla1()
@@ -127,9 +131,20 @@ void Tecla2()
 {
 	_hold = !_hold;
 }
+void FuncTimerA(void* param){
+    vTaskNotifyGiveFromISR(Medir_task_handle, pdFALSE);   
+	vTaskNotifyGiveFromISR(EncenderLEDs_task_handle, pdFALSE);   
+	vTaskNotifyGiveFromISR(ControlLCD_task_handle, pdFALSE);   
+}
 /*==================[external functions definition]==========================*/
 void app_main(void)
 {
+	    timer_config_t timer_medicion = {
+        .timer = TIMER_A,
+        .period = CONFIG_MEASUREMENT_PERIOD_US,
+        .func_p = FuncTimerA,
+        .param_p = NULL
+    };
 
     LedsInit();
     HcSr04Init(GPIO_3, GPIO_2);
@@ -138,9 +153,14 @@ void app_main(void)
 	SwitchActivInt(SWITCH_2, Tecla2, NULL);
     LcdItsE0803Init();
 
+	TimerInit(&timer_medicion);
+	
     xTaskCreate(&Medir, "Medir", 512, NULL, 5, &Medir_task_handle);
     xTaskCreate(&EncenderLEDs, "EncenderLEDs", 512, NULL, 5, &EncenderLEDs_task_handle);
     xTaskCreate(&ControlLCD, "ControlLCD", 512, NULL, 5, &ControlLCD_task_handle);
+
+
+	TimerStart(timer_medicion.timer);
 }
 
 /*==================[end of file]============================================*/
