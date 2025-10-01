5694ffadc80f756f0b9b128715175668d91f4796
Gastón Díaz <diazgastonj@gmail.com>
2025-10-01 09:09:45 -0300

    Proyecto guia2_ej2 completo con teclas de interrupción en vez de capturar teclas en intervalos de tiempo.

--- firmware/projects/guia2_ej2/main/guia2_ej2.c
+++ firmware/projects/guia2_ej2/main/guia2_ej2.c
@@ -2,7 +2,8 @@
  *
  * @section genDesc General Description
  *
- * This section describes how the program works.
+ * This example implements the guia2_ej2 (ultrasound distance meter) adding interrupting keys 
+ * insted of capturing keys in time intervals.
  *
  * <a href="https://drive.google.com/...">Operation Example</a>
  *
@@ -18,6 +19,7 @@
  * |   Date	    | Description                                    |
  * |:----------:|:-----------------------------------------------|
  * | 24/09/2025 | Document creation		                         |
+ * | 01/10/2025 | Functioning correctly		                     |
  *
  * @author Gastón Díaz (diazgastonj@gmail.com)
  *
@@ -26,14 +28,119 @@
 /*==================[inclusions]=============================================*/
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "led.h"
+#include "switch.h"
+#include "hc_sr04.h"
+#include "lcditse0803.h"
+#include "timer_mcu.h"
 /*==================[macros and definitions]=================================*/
-
+#define CONFIG_SWITCH_READ_PERIOD 100
+#define CONFIG_MEASUREMENT_PERIOD 1000
+#define CONFIG_BLINK_PERIOD_LED_3 500
 /*==================[internal data definition]===============================*/
+TaskHandle_t LeerTeclas_task_handle = NULL;
+TaskHandle_t Medir_task_handle = NULL;
+TaskHandle_t EncenderLEDs_task_handle = NULL;
+TaskHandle_t ControlLCD_task_handle = NULL;
 
+uint8_t _tecla;
+bool _medicionActivada = true;
+bool _hold = false;
+uint16_t _distanciaCentimetros = 0;
 /*==================[internal functions declaration]=========================*/
+static void Medir(void *pvParameter)
+{
+    while (true)
+    {
 
-/*==================[external functions definition]==========================*/
-void app_main(void){
-	printf("Hello world!\n");
+        if (_medicionActivada)
+            _distanciaCentimetros = HcSr04ReadDistanceInCentimeters();
+
+        vTaskDelay(CONFIG_MEASUREMENT_PERIOD / portTICK_PERIOD_MS);
+    }
 }
+
+static void EncenderLEDs(void *pvParameter)
+{
+    while (true)
+    {
+        if (_medicionActivada)
+        {
+            if (_distanciaCentimetros < 10)
+            {
+                LedOff(LED_1);
+                LedOff(LED_2);
+                LedOff(LED_3);
+            }
+            else if (_distanciaCentimetros < 20)
+            {
+                LedOn(LED_1);
+                LedOff(LED_2);
+                LedOff(LED_3);
+            }
+            else if (_distanciaCentimetros < 30)
+            {
+                LedOn(LED_1);
+                LedOn(LED_2);
+                LedOff(LED_3);
+            }
+            else
+            {
+                LedOn(LED_1);
+                LedOn(LED_2);
+                LedOn(LED_3);
+            }
+        }
+        else
+        {
+            LedOff(LED_1);
+            LedOff(LED_2);
+            LedOff(LED_3);
+        }
+
+        vTaskDelay(CONFIG_MEASUREMENT_PERIOD / portTICK_PERIOD_MS);
+    }
+}
+static void ControlLCD(void *pvParameter)
+{
+    while (true)
+    {
+        if (!_medicionActivada)
+        {
+            LcdItsE0803Off();
+        }
+        else if (!_hold)
+        {
+            LcdItsE0803Write(_distanciaCentimetros);
+        }
+        vTaskDelay(CONFIG_MEASUREMENT_PERIOD / portTICK_PERIOD_MS);
+    }
+}
+void Tecla1()
+{
+	_medicionActivada = !_medicionActivada;
+}
+void Tecla2()
+{
+	_hold = !_hold;
+}
+/*==================[external functions definition]==========================*/
+void app_main(void)
+{
+
+    LedsInit();
+    HcSr04Init(GPIO_3, GPIO_2);
+    SwitchesInit();
+	SwitchActivInt(SWITCH_1, Tecla1, NULL);
+	SwitchActivInt(SWITCH_2, Tecla2, NULL);
+    LcdItsE0803Init();
+
+    xTaskCreate(&Medir, "Medir", 512, NULL, 5, &Medir_task_handle);
+    xTaskCreate(&EncenderLEDs, "EncenderLEDs", 512, NULL, 5, &EncenderLEDs_task_handle);
+    xTaskCreate(&ControlLCD, "ControlLCD", 512, NULL, 5, &ControlLCD_task_handle);
+}
+
 /*==================[end of file]============================================*/
