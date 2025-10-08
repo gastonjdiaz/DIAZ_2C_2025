ed37419310fa8c99b4c7e6ebff9aebc0a33550b1
Gastón Díaz <diazgastonj@gmail.com>
2025-10-08 09:21:49 -0300

    Creo Guia2_ej4

---
+++ firmware/projects/guia2_ej4/main/guia2_ej4.c
@@ -0,0 +1,38 @@
+/*! @mainpage guia2_ej4
+ *
+ * @section Proyecto creación de osciloscopio
+ *
+ * 
+ *
+ * 
+ * @section hardConn Hardware Connection
+ *
+ * |    Peripheral  |   ESP32   	|
+ * |:--------------:|:--------------|
+ * | 	PIN_X	 	| 	GPIO_X		|
+ *
+ *
+ * @section changelog Changelog
+ *
+ * |   Date	    | Description                                    |
+ * |:----------:|:-----------------------------------------------|
+ * | 08/10/2025 | Document creation		                         |
+ *
+ * @author Gastón Díaz (diazgastonj@gmail.com)
+ *
+ */
+
+/*==================[inclusions]=============================================*/
+#include <stdio.h>
+#include <stdint.h>
+/*==================[macros and definitions]=================================*/
+
+/*==================[internal data definition]===============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[external functions definition]==========================*/
+void app_main(void){
+	printf("Hello world!\n");
+}
+/*==================[end of file]============================================*/
