d68d34c04f93142019a2929f9c221ee0e23f78f6
Gastón Díaz <diazgastonj@gmail.com>
2025-10-15 10:49:15 -0300

    guia2_ej4 finalizado, grafica ECG cargado en memoria, mediante el conversor DA y luego conectandolo al AD

--- firmware/projects/guia2_ej4/main/guia2_ej4.c
+++ firmware/projects/guia2_ej4/main/guia2_ej4.c
@@ -36,6 +36,18 @@
 #define CONFIG_SAMPLING_PERIOD_US 2000 // 2 ms -> 500Hz
 /*==================[internal data definition]===============================*/
 TaskHandle_t GenerarDatosEnvio_task_handle = NULL;
+TaskHandle_t EnviarDatoECG_task_handle = NULL;
+
+unsigned char ECG[] = {
+17,17,17,17,17,17,17,17,17,17,17,18,18,18,17,17,17,17,17,17,17,18,18,18,18,18,18,18,17,17,16,16,16,16,17,17,18,18,18,17,17,17,17,
+18,18,19,21,22,24,25,26,27,28,29,31,32,33,34,34,35,37,38,37,34,29,24,19,15,14,15,16,17,17,17,16,15,14,13,13,13,13,13,13,13,12,12,
+10,6,2,3,15,43,88,145,199,237,252,242,211,167,117,70,35,16,14,22,32,38,37,32,27,24,24,26,27,28,28,27,28,28,30,31,31,31,32,33,34,36,
+38,39,40,41,42,43,45,47,49,51,53,55,57,60,62,65,68,71,75,79,83,87,92,97,101,106,111,116,121,125,129,133,136,138,139,140,140,139,137,
+133,129,123,117,109,101,92,84,77,70,64,58,52,47,42,39,36,34,31,30,28,27,26,25,25,25,25,25,25,25,25,24,24,24,24,25,25,25,25,25,25,25,
+24,24,24,24,24,24,24,24,23,23,22,22,21,21,21,20,20,20,20,20,19,19,18,18,18,19,19,19,19,18,17,17,18,18,18,18,18,18,18,18,17,17,17,17,
+17,17,17
+
+} ;
 /*==================[internal functions declaration]=========================*/
 static void GenerarDatosEnvio(void *pvParameter)
 {
@@ -50,9 +62,22 @@ static void GenerarDatosEnvio(void *pvParameter)
 		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
 	}
 }
+static void EnviarDatoECG(void *pvParameter)
+{
+	while (true)
+	{
+		for (uint8_t i = 0; i < sizeof(ECG) ; i++)
+		{
+			AnalogOutputWrite(ECG[i]);
+			ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+		}
+				
+	}
+}
 void TimerFunc(void *parm)
 {
 	vTaskNotifyGiveFromISR(GenerarDatosEnvio_task_handle, pdFALSE);
+	vTaskNotifyGiveFromISR(EnviarDatoECG_task_handle, pdFALSE);
 }
 
 /*==================[external functions definition]==========================*/
@@ -82,7 +107,10 @@ void app_main(void)
 	UartInit(&my_uart);
 	AnalogInputInit(&analog_io_config);
 
-	xTaskCreate(&GenerarDatosEnvio, "ControlLCD", 512, NULL, 5, &GenerarDatosEnvio_task_handle);
+	AnalogOutputInit();
+
+	xTaskCreate(&GenerarDatosEnvio, "GenerarDatosEnvio", 512, NULL, 5, &GenerarDatosEnvio_task_handle);
+	xTaskCreate(&EnviarDatoECG, "EnviarDatoECG", 512, NULL, 5, &EnviarDatoECG_task_handle);
 
 	TimerStart(timer_medicion.timer);
 }
