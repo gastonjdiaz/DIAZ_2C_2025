6651314e9a84f45c4202809005a6414e0cffd0bb
Gastón Díaz <diazgastonj@gmail.com>
2025-12-02 10:19:27 -0300

    Genero carpeta de proyecto para evaluación final

---
+++ firmware/projects/20251202_DIAZ_FinalEProg/main/20251202_DIAZ_FinalEProg.c
@@ -0,0 +1,39 @@
+/*! @mainpage 20251202_DIAZ_FinalEProg
+ *
+ * @section genDesc General Description
+ *
+ * This section describes how the program works.
+ *
+ * <a href="https://drive.google.com/...">Operation Example</a>
+ *
+ * @section hardConn Hardware Connection
+ *
+ * |    Peripheral  |   ESP32   	|
+ * |:--------------:|:--------------|
+ * | 	PIN_X	 	| 	GPIO_X		|
+ *
+ *
+ * @section changelog Changelog
+ *
+ * |   Date	    | Description                                    |
+ * |:----------:|:-----------------------------------------------|
+ * | 02/12/2025 | Document creation		                         |
+ *
+ * @author Díaz, Gastón Jair (diazgastonj@gmail.com)
+ *
+ */
+
+/*==================[inclusions]=============================================*/
+#include <stdio.h>
+#include <stdint.h>
+/*==================[macros and definitions]=================================*/
+
+/*==================[internal data definition]===============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[external functions definition]==========================*/
+void app_main(void){
+	printf("Hello world!\n");
+}
+/*==================[end of file]============================================*/
